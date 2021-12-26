72ccb5eb1280d55338f7a2b96419e907c44077e8
Christof Ressi <info@christofressi.com>
2021-12-26 16:45:31 +0100

    clone: implement "parallel" method
    * if "parallel" is true, clone has its own signal context and DSP task queue.
    * child abstractions are scheduled as DSP tasks and finally joined.
    * the cloned abstractions don't need to know that they are being processed in parallel, we can carry out all necessary steps in clone_dsp().

--- src/d_ugen.c
+++ src/d_ugen.c
@@ -769,6 +769,27 @@ void ugen_addtask(t_dsptask *x)
     }
 }
 
+    /* used in clone_dsp() */
+t_dsptaskqueue * dsptaskqueue_push(t_dsptaskqueue *newqueue)
+{
+    t_dsptaskqueue *old;
+    if (!THIS->u_context || !((old = THIS->u_context->dc_dspqueue)))
+    {
+        bug("dsptaskqueue_push");
+        return 0;
+    }
+    THIS->u_context->dc_dspqueue = newqueue;
+    return old;
+}
+
+void dsptaskqueue_pop(t_dsptaskqueue *oldqueue)
+{
+    if (THIS->u_context && THIS->u_context->dc_dspqueue)
+        THIS->u_context->dc_dspqueue = oldqueue;
+    else
+        bug("dsptaskqueue_pop");
+}
+
 #endif /* PD_DSPTHREADS */
 
     /* get a new signal for the current context - used by clone~ object */
@@ -827,6 +848,16 @@ void ugen_start(void)
 int ugen_getsortno(void)
 {
     return (THIS->u_sortno);
+}
+
+t_int *ugen_getchain(void)
+{
+    return THIS->u_dspchain;
+}
+
+int ugen_getsize(void)
+{
+    return THIS->u_dspchainsize;
 }
 
 #if 0
