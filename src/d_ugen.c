f03fc9f8be6d792803794b6c0f567ed14c5f54bf
Christof Ressi <info@christofressi.com>
2021-12-27 01:41:56 +0100

    make clock scheduling methods thread-safe
    * add thread local variable for the current DSP thread index
    * if clocks are set/unset in the main thread (= 0), do as usual
    * if clocks are set/unset in a DSP helper thread (> 0), cache the desired time point and put the clock on a lock-free stack
    * at the end of dsp_tick() we take the list of deferred clocks and set/unset them for real.

--- src/d_ugen.c
+++ src/d_ugen.c
@@ -16,6 +16,9 @@
 #include "s_stuff.h"
 #include <stdlib.h>
 #include <stdarg.h>
+#if PD_DSPTHREADS
+# include "s_sync.h"
+#endif
 
 extern t_class *vinlet_class, *voutlet_class, *canvas_class, *text_class;
 
@@ -47,6 +50,7 @@ struct _instanceugen
     struct _dspcontext *u_context;
 #if PD_DSPTHREADS
     t_dsptaskqueue *u_dspqueue; /* toplevel DSP thread queue */
+    t_lockfree_stack u_clocks; /* only for the main queue */
 #endif
 };
 
@@ -60,6 +64,7 @@ void d_ugen_newpdinstance(void)
     THIS->u_signals = 0;
 #if PD_DSPTHREADS
     THIS->u_dspqueue = dsptaskqueue_new();
+    lockfree_stack_init(&THIS->u_clocks);
 #endif
 }
 
@@ -70,6 +75,14 @@ void d_ugen_freepdinstance(void)
 #endif
     freebytes(THIS, sizeof(*THIS));
 }
+
+#if PD_DSPTHREADS
+void clock_defer(t_clock *x)
+{
+    /* push to main queue */
+    lockfree_stack_push(&THIS->u_clocks, x);
+}
+#endif
 
 t_int *zero_perform(t_int *w)   /* zero out a vector */
 {
@@ -370,18 +383,28 @@ void dsp_addv(t_perfroutine f, int n, t_int *vec)
     THIS->u_dspchainsize = newsize;
 }
 
+#if PD_DSPTHREADS
+void clock_dispatch(t_clock *x);
+void dspthread_setindex(int index);
+#endif
+
 void dsp_tick(void)
 {
     if (THIS->u_dspchain)
     {
         t_int *ip;
     #if PD_DSPTHREADS
+        t_clock *c;
+        dspthread_setindex(0); /* just to be sure */
         dsptaskqueue_reset(THIS->u_dspqueue);
     #endif
         for (ip = THIS->u_dspchain; ip; ) ip = (*(t_perfroutine)(*ip))(ip);
         THIS->u_phase++;
     #if PD_DSPTHREADS
         dsptaskqueue_join(THIS->u_dspqueue);
+        /* dispatch deferred clocks */
+        if ((c = lockfree_stack_release(&THIS->u_clocks)))
+            clock_dispatch(c);
     #endif
     }
 }
